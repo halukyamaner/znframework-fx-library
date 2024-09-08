db14d580c2c152db8fb4d2e65fc767d43b27b094
Haluk YAMANER <8643150+halukyamaner@users.noreply.github.com>
2024-09-08 17:25:53 +0400

    Draft: FX Library 1.0

---
+++ FX.php
@@ -0,0 +1,38 @@
+<?php class FX
+{
+    /*
+     * FX Library
+     * Author: Haluk YAMANER
+     * Email: haluk@halukyamaner.com
+     * Web: https://www.halukyamaner.com
+     * Version: 1.0
+     *
+     * FX::Output($Data)
+     * FX::Translate($Term)
+     * FX::Settings($ConfigName)
+     * FX::UserData($Column)
+     * FX::Avg($Column, $Table)
+     * FX::Sum($Column, $Table)
+     * FX::CountRows($Table)
+     * FX::WhereCount($Table, $Column, $Value)
+     * FX::WhereData($Table, $Column, $Value, $Data)
+     * FX::GetValue($Table, $Value, $ID)
+     * FX::StringCheck($String, $Word)
+     * FX::LimitChars($String, $Limit)
+     * FX::LimitWords($String, $Limit)
+     * FX::LinkChange($NewURL)
+     * FX::CheckLogin($Redirect)
+     * FX::CheckFile($FileName)
+     * FX::CheckURI($Param, $Redirect)
+     * FX::CheckExists($Table, $Column, $Value)
+     * FX::CheckURL($URL = NULL)
+     * FX::Calculate($Value1, $Value2)
+     * FX::FolderSize($Directory) 
+     * FX::FindLinks($Text)
+     * FX::HexRead($File)
+     * FX::DateConvert($Date)
+     * FX::DayDifference($StartDate, $EndDate)
+     * FX::Flag($CountryCode)
+     * FX::CountryName($CountryCode)
+     * FX::MaskWords($Input)
+    */
